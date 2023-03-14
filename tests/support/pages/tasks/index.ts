9a4b6e1abe148544579c9fd6fe8f52f9cb5394c8
Cristian <cristianlohn@hotmail.com>
2023-03-14 17:55:53 -0300

    Adição de screenshot em caso de erro, e de navegadores mobile

--- tests/support/pages/tasks/index.ts
+++ tests/support/pages/tasks/index.ts
@@ -1,29 +1,51 @@
-import { Page, expect } from '@playwright/test'
+import { Page, Locator, expect } from '@playwright/test'
 import { TaskModel } from '../../../fixtures/task.model'
 
 
 export class TasksPage {
     readonly page: Page
+    readonly inputTaskName: Locator
 
     constructor(page: Page) {
         this.page = page
+        this.inputTaskName = page.locator('input[class*=InputNewTask]')
     }
 
     async go() {
-        await this.page.goto('http://localhost:3000')
+        await this.page.goto('/')
     }
 
 
-    async create(task: TaskModel) {
-        const inputTaskName = this.page.locator('input[class*=InputNewTask]')
-        await inputTaskName.fill(task.name)
+    async createWithButton(task: TaskModel) {
+        await this.inputTaskName.fill(task.name)
 
         await this.page.click('css=button >> text=Create')
+    }
+
+    async createWithPressEnter(task: TaskModel) {
+        await this.inputTaskName.fill(task.name)
+
+        await this.inputTaskName.press('Enter')
+    }
+
+    async toggle(taskName: string) {
+        const target = this.page.locator(`//p[text()="${taskName}"]/../button[contains(@class,"Toggle")]`)
+        await target.click()
+    }
+
+    async remove(taskName: string) {
+        const target = this.page.locator(`//p[text()="${taskName}"]/../button[contains(@class,"Delete")]`)
+        await target.click()
     }
 
     async shouldHaveText(taskName: string) {
         const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
         await expect(target).toHaveText(taskName)
+    }
+
+    async shouldNotExist(taskName: string) {
+        const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
+        await expect(target).not.toBeVisible()
     }
 
     async alertHaveText(text: string) {
@@ -34,6 +56,11 @@ export class TasksPage {
     async shouldToBeVisible(taskName: string) {
         const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
         await expect(target).toBeVisible()
+    }
+
+    async shouldBeDone(taskName: string) {
+        const target = this.page.getByText(taskName)
+        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
     }
 
 }
--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,74 +1,111 @@
-import { test, expect } from '@playwright/test'
+import { test, expect, request } from '@playwright/test'
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './support/pages/tasks'
+import data from './fixtures/tasks.json'
 
-//Validar criação de tarefas através do botão de criação
-test('deve poder cadastrar uma nova tarefa clicando no botão adicionar', async ({ page, request }) => {
-    const tasksPage: TasksPage = new TasksPage(page)
+let tasksPage: TasksPage
 
-    // Dado que eu tenho uma nova tarefa
-    const task: TaskModel = {
-        name: 'Criar tarefa usando botão',
-        is_done: false
-    }
-
-    await deleteTaskByHelper(request, task.name)
-
-    // E estou na página de cadastro
-    await tasksPage.go()
-
-    // Quando faço o cadastro dessa tarefa usando o botão
-    await tasksPage.create(task)
-
-    // Então essa tarefa deve ser exibida na lista
-    await tasksPage.shouldHaveText(task.name)
+test.beforeEach(({ page }) => {
+    tasksPage = new TasksPage(page)
 })
 
-//Validar criação de tarefas através da tecla enter
-test('deve poder cadastrar uma nova tarefa teclando enter', async ({ page, request }) => {
-    const tasksPage: TasksPage = new TasksPage(page)
 
-    // Dado que eu tenho uma nova tarefa
-    const task: TaskModel = {
-        name: 'Criar tarefa usando tecla Enter',
-        is_done: false
-    }
+test.describe('cadastro', () => {
+    //Validar criação de tarefas através do botão de criação
+    test('deve poder cadastrar uma nova tarefa clicando no botão adicionar', async ({ request }) => {
 
-    await deleteTaskByHelper(request, task.name)
+        // Dado que eu tenho uma nova tarefa
+        const task = data.success as TaskModel
 
-    // E estou na página de cadastro
-    await tasksPage.go()
+        await deleteTaskByHelper(request, task.name)
 
-    // Quando faço o cadastro dessa tarefa usando a tecla enter
-    await tasksPage.create(task)
+        // E estou na página de cadastro
+        await tasksPage.go()
 
-    // Então essa tarefa deve ser exibida na lista
-    await tasksPage.shouldToBeVisible(task.name)
+        // Quando faço o cadastro dessa tarefa usando o botão
+        await tasksPage.createWithButton(task)
+
+        // Então essa tarefa deve ser exibida na lista
+        await tasksPage.shouldHaveText(task.name)
+    })
+
+    //Validar criação de tarefas através da tecla enter
+    test('deve poder cadastrar uma nova tarefa teclando enter', async ({ request }) => {
+
+        // Dado que eu tenho uma nova tarefa
+        const task = data.sundingByEnter as TaskModel
+
+        await deleteTaskByHelper(request, task.name)
+
+        // E estou na página de cadastro
+        await tasksPage.go()
+
+        // Quando faço o cadastro dessa tarefa usando a tecla enter
+        await tasksPage.createWithPressEnter(task)
+
+        // Então essa tarefa deve ser exibida na lista
+        await tasksPage.shouldToBeVisible(task.name)
+    })
+
+    //Validar tarefas duplicadas
+    test('não deve permitir tarefa duplicada', async ({ request }) => {
+
+        // Dado que eu tenho uma nova tarefa
+        const task = data.duplicate as TaskModel
+
+        //Remove tasks criadas
+        await deleteTaskByHelper(request, task.name)
+
+        //Cria task a ser duplicada
+        await postTask(request, task)
+
+        //E estou na página de cadastro
+        await tasksPage.go()
+
+        // Quando faço um novo cadastro com o mesmo nome de uma task existente
+        await tasksPage.createWithButton(task)
+
+        // Então devo receber um aviso de que não posso criar tarefas duplicadas
+        await tasksPage.alertHaveText('Task already exists!')
+    })
+
+    test('Campo obrigatório', async () => {
+        const task = data.required as TaskModel
+
+        await tasksPage.go()
+        await tasksPage.createWithButton(task)
+
+        const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+        expect(validationMessage).toEqual('This is a required field')
+
+    })
 })
 
-//Validar tarefas duplicadas
-test('não deve permitir tarefa duplicada', async ({ page, request }) => {
-    const tasksPage: TasksPage = new TasksPage(page)
+test.describe('atualização', () => {
+    test('Deve concluir uma tarefa', async ({ request }) => {
+        const task = data.update as TaskModel
 
-    // Dado que eu tenho uma nova tarefa
-    const task: TaskModel = {
-        name: 'Criar tarefa duplicada',
-        is_done: false
-    }
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
 
-    //Remove tasks criadas
-    await deleteTaskByHelper(request, task.name)
-
-    //Cria task a ser duplicada
-    await postTask(request, task)
-
-    //E estou na página de cadastro
-    await tasksPage.go()
-
-    // Quando faço um novo cadastro com o mesmo nome de uma task existente
-    await tasksPage.create(task)
-
-    // Então devo receber um aviso de que não posso criar tarefas duplicadas
-    await tasksPage.alertHaveText('Task already exists!')
+        await tasksPage.go()
+        await tasksPage.toggle(task.name)
+        await tasksPage.shouldBeDone(task.name)
+    })
 })
+
+test.describe('exclusão', () => {
+    test('Deve excluir uma tarefa', async ({ request }) => {
+        const task = data.delete as TaskModel
+
+        await deleteTaskByHelper(request, task.name)
+        await postTask(request, task)
+
+        await tasksPage.go()
+        await tasksPage.remove(task.name)
+        await tasksPage.shouldNotExist(task.name)
+    })
+})
+
+
